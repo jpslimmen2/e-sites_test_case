b5ce9e0bb27931feae7a2b15be51593ee3c468b5
jpslimmen2 <jpslimmen@gmail.com>
2022-11-04 23:04:36 +0100

    Update ReservationController.php

--- src/Controller/ReservationController.php
+++ src/Controller/ReservationController.php
@@ -27,52 +27,45 @@ class ReservationController extends AbstractController
      * @OA\Tag(name="solution")
      */
     public function solution(int $persons) : response{
-        //gets all seats
+        
         $allSeats = $this->seatRepository->findAll();
 
-        //filters all seats on availability
         $availableSeats = array_filter($allSeats, function($seat){
             return !$seat->isOccupied();
         });
 
-        // returns null if amount of persons is higher than available seats
         if (count($availableSeats) < $persons){
             return $this->json(null);
         }
 
-        $hashArray = []; // array with empty seats
-        $groupsArray = []; // array with grouped seats
+        $hashArray = []; 
+        $groupsArray = []; 
 
-        // foreach for creating groups of free seats
         foreach ($allSeats as $key => $seat){
 
-            //puts seat in hashArray when occupation is false and breaks if amount is higher or equal of amount of persons
             if ($seat->isOccupied() === false){
                 array_push($hashArray, $seat);
                 if (count($hashArray)  >= $persons){
                     break;
                 }
             }
-            //pushes the empty seats into a group array and resets it to create a new group
+           
             else if(count($hashArray) > 0){
                 array_push($groupsArray, $hashArray);
                 $hashArray = [];
                 continue;
             }
 
-            //if the index is eqeul to allseats -1 and hashArray still has items create a new group for it.
             if ($key === count($allSeats) -1 && count($hashArray) > 0){
                 array_push($groupsArray, $hashArray);
                 $hashArray = [];
             }
         }
 
-        // returns the array when a big enough gap has been found where everyone can fit in.
         if (count($hashArray) > 0){
             return $this->json($hashArray);
         }
 
-        //assigns the group to different seats when there isn't enough space for everyone next to each other
         for ($i = 0; $i < count($groupsArray); $i++){
             $foundGroup = $groupsArray[$i];
 
@@ -89,7 +82,7 @@ class ReservationController extends AbstractController
                 break;
             }
         }
-        //returns json array of available seats
+        
         return $this->json($hashArray);
     }
 }
