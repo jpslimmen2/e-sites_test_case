7e826d5bad36b21f5d436cae9e88ffc3e582283b
jpslimmen <jpslimmen@gmail.com>
2022-11-10 05:36:45 +0100

    WIP refactor

--- src/Controller/ReservationController.php
+++ src/Controller/ReservationController.php
@@ -3,11 +3,15 @@
 namespace App\Controller;
 
 use App\Repository\SeatRepository;
+use App\Service\SeatAllocator;
 use OpenApi\Annotations as OA;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ *
+ */
 class ReservationController extends AbstractController
 {
     private SeatRepository $seatRepository;
@@ -15,8 +19,6 @@ class ReservationController extends AbstractController
     public function __construct(SeatRepository $seatRepository){
         $this->seatRepository = $seatRepository;
     }
-
-    #[Route('api/solution/{persons}', name: 'app_reservation', methods: 'GET')]
     /**
      * @OA\Parameter(
      *     name="persons",
@@ -25,72 +27,28 @@ class ReservationController extends AbstractController
      *     @OA\Schema(type="integer")
      * )
      * @OA\Tag(name="solution")
+     *
+     * @param SeatAllocator $seatAllocator
+     * @param int           $amountOfPersons
+     *
+     * @return Response
      */
-    public function solution(int $persons) : response{
-        //gets all seats
-        $allSeats = $this->seatRepository->findAll();
+    #[Route('api/solution/{amountOfPersons}', name: 'app_reservation', methods: 'GET')]
 
-        //filters all seats on availability
-        $availableSeats = array_filter($allSeats, function($seat){
-            return !$seat->isOccupied();
-        });
+    public function solution(SeatAllocator $seatAllocator, int $amountOfPersons) : response
+    {
+        $seats = $this->seatRepository->findAll();
 
-        // returns null if amount of persons is higher than available seats
-        if (count($availableSeats) < $persons){
-            return $this->json(null);
+        $availableSeats = $seatAllocator->getAvailableSeats($seats);
+
+        if ($seatAllocator->checkAvailability($availableSeats ,$amountOfPersons))
+        {
+            $groupedSeats = $seatAllocator->groupSeats($seats, $amountOfPersons);
+            $assignedSeats = $seatAllocator->assignSeats($groupedSeats, $amountOfPersons);
+            return $this->json($assignedSeats);
         }
 
-        $hashArray = []; // array with empty seats
-        $groupsArray = []; // array with grouped seats
-
-        // foreach for creating groups of free seats
-        foreach ($allSeats as $key => $seat){
-
-            //puts seat in hashArray when occupation is false and breaks if amount is higher or equal of amount of persons
-            if ($seat->isOccupied() === false){
-                array_push($hashArray, $seat);
-                if (count($hashArray)  >= $persons){
-                    break;
-                }
-            }
-            //pushes the empty seats into a group array and resets it to create a new group
-            else if(count($hashArray) > 0){
-                array_push($groupsArray, $hashArray);
-                $hashArray = [];
-                continue;
-            }
-
-            //if the index is eqeul to allseats -1 and hashArray still has items create a new group for it.
-            if ($key === count($allSeats) -1 && count($hashArray) > 0){
-                array_push($groupsArray, $hashArray);
-                $hashArray = [];
-            }
-        }
-
-        // returns the array when a big enough gap has been found where everyone can fit in.
-        if (count($hashArray) > 0){
-            return $this->json($hashArray);
-        }
-
-        //assigns the group to different seats when there isn't enough space for everyone next to each other
-        for ($i = 0; $i < count($groupsArray); $i++){
-            $foundGroup = $groupsArray[$i];
-
-            for ($y = 0; $y < count($foundGroup); $y++){
-                $seat = $foundGroup[$y];
-                array_push($hashArray,$seat);
-                $persons--;
-
-                if ($persons <= 0){
-                    break;
-                }
-            }
-            if ($persons <= 0){
-                break;
-            }
-        }
-        //returns json array of available seats
-        return $this->json($hashArray);
+        return $this->json(null);
     }
 }
 
---
+++ src/Service/SeatAllocator.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace App\Service;
+
+class SeatAllocator
+{
+    /**
+     * @var array
+     */
+    private array $hashArray = [];
+
+    /**
+     * @var array
+     */
+    private array $groupsArray = [];
+
+    /**
+     * @var array
+     */
+    private array $assignedSeats = [];
+
+    /**
+     * @description filters provided seats based on occupation.
+     * @param array $seats
+     *
+     * @return array
+     */
+    public function getAvailableSeats(array $seats) : array
+    {
+        return array_filter($seats, function($seat)
+        {
+            return !$seat->isOccupied();
+        });
+    }
+
+    /**
+     * @description checks if there is enough available seats for the amount of persons.
+     * @param array $availableSeats
+     * @param int   $amountOfPersons
+     *
+     * @return null|bool
+     */
+    public function checkAvailability(array $availableSeats, int $amountOfPersons): ?bool
+    {
+        if (count($availableSeats) < $amountOfPersons)
+        {
+            return null;
+        }
+        return true;
+    }
+
+    /**
+     * @description Make seat groups.
+     * @param array $seats
+     * @param int   $AmountOfPersons
+     *
+     * @return array
+     */
+    public function groupSeats(array $seats, int $AmountOfPersons) : array
+    {
+        foreach ($seats as $key => $seat){
+            if ($seat->isOccupied() === false)
+            {
+                $this->hashArray[] = $seat;
+                if (count($this->hashArray)  >= $AmountOfPersons)
+                {
+                    break;
+                }
+            }
+
+            else if(count($this->hashArray) > 0)
+            {
+                $this->groupsArray[] = $this->hashArray;
+                $this->hashArray = [];
+                continue;
+            }
+
+            if ($key === count($seats) -1 && count($this->hashArray) > 0)
+            {
+                $this->groupsArray[] = $this->hashArray;
+                $this->hashArray = [];
+            }
+        }
+        return $this->groupsArray;
+    }
+
+
+    /**
+     * @description Assigns the person to the best suiting seat.
+     *
+     * @param array $groupedSeats
+     * @param int   $amountOfPersons
+     *
+     * @return array
+     */
+    public function assignSeats(array $groupedSeats ,int $amountOfPersons) : array
+    {
+        $this->hashArray = [];
+        for ($i = 0; $i < count($groupedSeats); $i++)
+        {
+            $foundGroup = $groupedSeats[$i];
+            for ($y = 0; $y < count($foundGroup); $y++)
+            {
+                $seat = $foundGroup[$y];
+                $this->assignedSeats[] = $seat;
+                $amountOfPersons--;
+
+                if ($amountOfPersons <= 0)
+                {
+                    break;
+                }
+            }
+            if ($amountOfPersons <= 0)
+            {
+                break;
+            }
+        }
+
+        return $this->assignedSeats;
+    }
+
+}
